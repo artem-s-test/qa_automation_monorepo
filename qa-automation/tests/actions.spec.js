1a08ca7bcef1fdd7d40e4de5f7fc7c119b52c87b
Artem Shoiko <shoiko.a.s@gmail.com>
2026-07-26 22:19:24 +0300

    Actions

---
+++ qa-automation/tests/actions.spec.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('click seach', async ({ page }) => {
+    await page.goto('http://localhost:5173/')
+    await page.getByRole('button', {name: "Search"}).dblclick()
+    await page.getByRole('article').first().click({button: 'right'})
+    await page.getByRole('article').first().click({modifiers: ['Control']})
+    await page.getByRole('article').first().click({position: {
+        x: 100,
+        y: 100
+    }})
+});
+
+test('login page', async ({ page }) => {
+    await page.goto('http://localhost:5173/auth/login')
+    await page.locator('#email').fill('goit@gmail.com')
+    await page.locator('#password').fill('Foodies2025!')
+});
+
+test('enter search request', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    await page.getByPlaceholder('Search recipes').fill('avocado')
+    await page.getByPlaceholder('Search recipes').press('Enter')
+});
+
+test('checkbox demo', async ({ page }) => {
+    await page.goto('http://localhost:5173/auth/register')
+    await page.locator('#terms').check()
+});
+
+test('add receipe', async ({ page }) => {
+    await page.goto('http://localhost:5173/auth/login')
+    await page.locator('#email').fill('goit@gmail.com')
+    await page.locator('#password').fill('Foodies2025!')
+    await page.getByRole('button', {name: "Login"}).click()
+    await page.locator('a[href="/add-recipe"]').click()
+    await page.locator('#photoInput').setInputFiles([])
+    await page.pause()
+});
