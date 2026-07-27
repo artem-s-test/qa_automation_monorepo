99554fca336590e3ea20e345823bd20c5f8e2ced
Artem Shoiko <shoiko.a.s@gmail.com>
2026-07-27 14:26:48 +0300

    Assertions

---
+++ qa-automation/tests/assertions.spec.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('login page', async ({ page }) => {
+    await page.goto('http://localhost:5173/auth/login')
+    await expect(page.locator('#email')).toBeVisible()
+    await expect(page.locator('button[type="submit"]')).toBeEnabled()
+    await page.locator('#email').fill('goit@gmail.com')
+    await page.locator('#password').fill('Foodies2025!')
+    await expect(page.locator('#email')).toHaveValue('goit@gmail.com')
+    await expect(page.locator('#password')).toHaveValue('Foodies2025!')
+    await page.getByRole('button', {name: "Login"}).click()
+    await expect(page.getByText('Invalid credentials')).not.toBeVisible()
+    await expect(page).toHaveURL('http://localhost:5173/register')
+    await expect(page).not.toHaveURL('http://localhost:5173/auth/login')
+
+    await expect(page.getByRole('article').first()).toBeVisible()
+    const count = await page.getByRole('article').count()
+    expect(count).toBeGreaterThan(0)
+    // toContainText
+    // toHaveCount
+});
