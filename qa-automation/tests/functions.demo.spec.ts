0814f6723501a1747bb460b9f4bd8dfc1c5994db
Artem Shoiko <shoiko.a.s@gmail.com>
2026-08-12 14:41:20 +0300

    Update file

--- qa-automation/tests/functions.demo.spec.ts
+++ qa-automation/tests/functions.demo.spec.ts
@@ -1,19 +1,17 @@
-import { test, expect } from '@playwright/test';
-import { generateRandomEmail, calculateDiscountedPrice } from '../helpers'
+import { test, expect } from "@playwright/test";
+import { generateRandomEmail, calculateDiscountedPrice } from "../helpers";
 
-
-test('функції', async ({ page }) => {
-   
-    const finalPrice: number = calculateDiscountedPrice(299, 10)
-    console.log(finalPrice)
+test("функції", async ({ page }) => {
+  const finalPrice: number = calculateDiscountedPrice(299, 10);
+  console.log(finalPrice);
 });
 
-test('реєстрація нового користувача', async ({ page }) => {
-    const email: string = generateRandomEmail('user')
-    console.log('Реєструю нового користувача: ', email)
+test("реєстрація нового користувача", async ({ page }) => {
+  const email: string = generateRandomEmail("user");
+  console.log("Реєструю нового користувача: ", email);
 });
 
-test('перевірка ціни тарифу зі знижкою', async ({ page }) => {
-    const finalPrice: number = calculateDiscountedPrice(499, 10)
-    expect(finalPrice).toBe(449.1)
-})
+test("перевірка ціни тарифу зі знижкою", async ({ page }) => {
+  const finalPrice: number = calculateDiscountedPrice(499, 10);
+  expect(finalPrice).toBe(449.1);
+});
