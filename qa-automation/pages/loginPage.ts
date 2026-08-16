5eac7bf308f88cc21c9a37126757cc15901f88d2
Artem Shoiko <shoiko.a.s@gmail.com>
2026-08-16 13:50:00 +0300

    Update POM for login page

--- qa-automation/pages/loginPage.ts
+++ qa-automation/pages/loginPage.ts
@@ -1,24 +1,21 @@
 import { Page, Locator } from '@playwright/test'
 
 export class LoginPage {
-  #emailInput: string
-  #password: string
-  #loginButton: Locator
+  emailInput: Locator
+  password: Locator
+  loginButton: Locator
   page: Page
 
   constructor(page: Page) {
     this.page = page;
-    this.#emailInput = '#email';
-    this.#password = '#password';
-    this.#loginButton = page.locator('button[type="submit"]')
+    this.emailInput = page.locator('#email');
+    this.password = page.locator('#password');
+    this.loginButton = page.locator('button[type="submit"]')
   }
-
-  #fillField(selector: string, value: string){
-    return this.page.fill(selector, value)
-  }
+  
   async login(email: string, password: string): Promise<void> {
-    await this.#fillField(this.#emailInput, email)
-    await this.#fillField(this.#password, password)
-    await this.#loginButton.click()
+    await this.emailInput.fill(email)
+    await this.password.fill(password)
+    await this.loginButton.click()
   }
 }
